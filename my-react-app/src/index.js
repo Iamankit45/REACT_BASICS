fae3d2f4d2b33a018c19407dfcc83a80cf5ce958
ANKIT <kumarankit0327@gmail.com>
2023-03-04 20:09:48 +0530

    added photos and attributes

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -38,12 +38,23 @@ import ReactDOM from "react-dom";
 const curdate=new Date().toLocaleDateString();
 const curtime=new Date().toLocaleTimeString();
 
+const img1="https://picsum.photos/200/300";
+const img2="https://picsum.photos/210/300";
+const img3="https://picsum.photos/220/300";
+const img4="https://picsum.photos/280/300";
 ReactDOM.render(
   <>
-  <h2>{`current date is ${curdate}`}</h2>
+  <h2 contentEditable="true">{`current date is ${curdate}`}</h2>
   <h3>{`current time is ${curtime}`}</h3>
- 
-  
+ <img src={img1} alt="random images"/>
+ <img src={img2} alt="random images"/>
+ <img src={img3} alt="random images"/>
+
+<a href="https://picsum.photos/">
+<img src={img4} alt="random images"/>
+</a>
+
+
   </>,document.getElementById("root")
   );
 
