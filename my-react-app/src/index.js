088c3412deb3551475a55de0599acd9e975f2ee8
ANKIT <kumarankit0327@gmail.com>
2023-03-04 19:33:39 +0530

    learned expression in JSX

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const s1="dark";
+const s2="GAME OF THRONES";
+const s3="house of dragons";
+
+
 ReactDOM.render(
   <><h1>Ankit netflix Pick</h1><p>here is the name of the series</p><ol>
-<li>DARK</li>
-<li>GAME OF THRONES</li>
+<li>{s1}</li>
+<li>{s2}</li>
 <li>THIRTEENTH FLOOR</li>
 <li>WIZARD OF TRAGON</li>
-<li>HOUSE  OF DRAGON</li>
+<li>{s3}</li>
   </ol></>,
   document.getElementById("root")
 );
