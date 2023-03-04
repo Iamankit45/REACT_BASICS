cddc431adc7e1ca94a4f04d9ff0bebe2ba2cce6e
ANKIT <kumarankit0327@gmail.com>
2023-03-04 19:40:35 +0530

    used template literals

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -6,6 +6,9 @@ const s2="GAME OF THRONES";
 const s3="house of dragons";
 
 
+const fname="ankit";
+const lname="kumar";
+
 ReactDOM.render(
   <><h1>Ankit netflix Pick</h1><p>here is the name of the series</p><ol>
 <li>{s1}</li>
@@ -16,3 +19,14 @@ ReactDOM.render(
   </ol></>,
   document.getElementById("root")
 );
+
+
+
+ReactDOM.render(
+<>
+<h2>{`my firstname is ${fname} and my lastname is ${lname}`}</h2>
+
+</>,document.getElementById("root")
+);
+
+
