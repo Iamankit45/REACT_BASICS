3cce3ef77e1e1045847e2cd333ecfa4c162c2f71
ANKIT <kumarankit0327@gmail.com>
2023-03-04 19:50:56 +0530

    finding current date and time

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,32 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const s1="dark";
-const s2="GAME OF THRONES";
-const s3="house of dragons";
+// const s1="dark";
+// const s2="GAME OF THRONES";
+// const s3="house of dragons";
 
 
-const fname="ankit";
-const lname="kumar";
+// const fname="ankit";
+// const lname="kumar";
+
+// ReactDOM.render(
+//   <><h1>Ankit netflix Pick</h1><p>here is the name of the series</p><ol>
+// <li>{s1}</li>
+// <li>{s2}</li>
+// <li>THIRTEENTH FLOOR</li>
+// <li>WIZARD OF TRAGON</li>
+// <li>{s3}</li>
+//   </ol></>,
+//   document.getElementById("root")
+// );
+
+
+
+// ReactDOM.render(
+// <>
+// <h2>{`my firstname is ${fname} and my lastname is ${lname}`}</h2>
+
+// <p1>{`sum of 2 nad 3 is ${2+3}`}</p1>
+
+// </>,document.getElementById("root")
+// );
+
+
+
+//finding current date and time 
+
+const curdate=new Date().toLocaleDateString();
+const curtime=new Date().toLocaleTimeString();
 
 ReactDOM.render(
-  <><h1>Ankit netflix Pick</h1><p>here is the name of the series</p><ol>
-<li>{s1}</li>
-<li>{s2}</li>
-<li>THIRTEENTH FLOOR</li>
-<li>WIZARD OF TRAGON</li>
-<li>{s3}</li>
-  </ol></>,
-  document.getElementById("root")
-);
+  <>
+  <h2>{`current date is ${curdate}`}</h2>
+  <h3>{`current time is ${curtime}`}</h3>
+ 
+  
+  </>,document.getElementById("root")
+  );
 
-
-
-ReactDOM.render(
-<>
-<h2>{`my firstname is ${fname} and my lastname is ${lname}`}</h2>
-
-</>,document.getElementById("root")
-);
 
 
