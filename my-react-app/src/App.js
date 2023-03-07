c967b5271203d6d3d50dc24a732b72704ad3d666
ANKIT <kumarankit0327@gmail.com>
2023-03-07 23:53:41 +0530

    added app.jsx

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,24 +1,101 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from "react-dom";
+import Heading from "./Heading";
+import List from "./List";
+import Para from "./Para";
+
+
 
 function App() {
+
+  const heading2={
+    color:"rgb(255, 0, 0)",
+    textAlign:"center",
+    margin:"50px",
+    fontFamily: "'Dancing Script', cursive"
+    
+    
+    
+    }
+
+
+    const curdate=new Date().toLocaleDateString();
+      const curtime=new Date().toLocaleTimeString();
+      
+      const img1="https://picsum.photos/200/300";
+      const img2="https://picsum.photos/210/300";
+      const img3="https://picsum.photos/220/300";
+      const img4="https://picsum.photos/280/300";
+      
+      
+      let currdate=new Date();
+      let currtime=currdate.getHours();
+      let greeting='';
+      const cssStyle={};
+      
+      if (currtime>=1&&currtime<12) {
+        
+        greeting="Good Morning";
+        cssStyle.color='green';
+      }else if(currtime>=12&&currtime<19){
+        greeting="Good Afternoon";
+        cssStyle.color='yellow';
+      }else{
+        greeting="Good night";
+        cssStyle.color='blue';
+      }
+      
+      
+      
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    
+      
+      
+      
+      
+      ReactDOM.render(
+        <>
+        
+        <h2 className="heading1">{`current date is ${curdate}`}</h2>
+        <h3 style={heading2}>{`current time is ${curtime}`}</h3>
+      
+        <div className="img_sec">
+       <img src={img1} alt="random images"/>
+       <img src={img2} alt="random images"/>
+       <img src={img3} alt="random images"/>
+      
+      <a href="https://picsum.photos/">
+      <img src={img4} alt="random images"/>
+      </a>
+      </div>
+      
+      
+      
+      
+      <div className="heading3">
+      
+      <h1>Hello sir ,<span style={cssStyle}>{greeting}</span></h1>
+      
+      
+        <Heading/>
+        <Para/>
+      
+        <List/>
+        
+      </div>
+      
+      
+      
+      
+      
+        </>,document.getElementById("root")
+        )
+      
+      
   );
 }
 
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,132 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Heading from "./Heading";
-import List from "./List";
-import Para from "./Para";
 
-
-
-
-// const s1="dark";
-// const s2="GAME OF THRONES";
-// const s3="house of dragons";
-
-
-// const fname="ankit";
-// const lname="kumar";
-
-// ReactDOM.render(
-//   <><h1>Ankit netflix Pick</h1><p>here is the name of the series</p><ol>
-// <li>{s1}</li>
-// <li>{s2}</li>
-// <li>THIRTEENTH FLOOR</li>
-// <li>WIZARD OF TRAGON</li>
-// <li>{s3}</li>
-//   </ol></>,
-//   document.getElementById("root")
-// );
-
-
-
-// ReactDOM.render(
-// <>
-// <h2>{`my firstname is ${fname} and my lastname is ${lname}`}</h2>
-
-// <p1>{`sum of 2 nad 3 is ${2+3}`}</p1>
-
-// </>,document.getElementById("root")
-// );
-
-
-
-//finding current date and time 
-
-// .heading1{
-
-//   color:rgb(255, 0, 0);
-//   text-align: center;
-//   margin:50px;
-//   font-family: 'Dancing Script', cursive;
-  
-// }
-
-const heading2={
-color:"rgb(255, 0, 0)",
-textAlign:"center",
-margin:"50px",
-fontFamily: "'Dancing Script', cursive"
-
-
-
-}
-
-
-const curdate=new Date().toLocaleDateString();
-const curtime=new Date().toLocaleTimeString();
-
-const img1="https://picsum.photos/200/300";
-const img2="https://picsum.photos/210/300";
-const img3="https://picsum.photos/220/300";
-const img4="https://picsum.photos/280/300";
-
-
-let currdate=new Date();
-let currtime=currdate.getHours();
-let greeting='';
-const cssStyle={};
-
-if (currtime>=1&&currtime<12) {
-  
-  greeting="Good Morning";
-  cssStyle.color='green';
-}else if(currtime>=12&&currtime<19){
-  greeting="Good Afternoon";
-  cssStyle.color='yellow';
-}else{
-  greeting="Good night";
-  cssStyle.color='blue';
-}
+import App from "./App";
 
 
 
 
 ReactDOM.render(
-  <>
+<>
+<App/>
+
+
+</>,document.getElementById("root"));
+
+
+
+
   
-  <h2 className="heading1">{`current date is ${curdate}`}</h2>
-  <h3 style={heading2}>{`current time is ${curtime}`}</h3>
-
-  <div className="img_sec">
- <img src={img1} alt="random images"/>
- <img src={img2} alt="random images"/>
- <img src={img3} alt="random images"/>
-
-<a href="https://picsum.photos/">
-<img src={img4} alt="random images"/>
-</a>
-</div>
 
 
-
-
-<div className="heading3">
-
-<h1>Hello sir ,<span style={cssStyle}>{greeting}</span></h1>
-
-
-  <Heading/>
-  <Para/>
-
-  <List/>
-  
-</div>
-
-
-
-
-
-  </>,document.getElementById("root")
-  );
 
 
 
